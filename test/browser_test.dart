adb79afa8f2257ff7811c6d386002009d232f9dc
nickfloros <nickfloros@gmail.com>
2013-06-14 23:42:12 +0100

    adding test cases

---
+++ test/browser_test.dart
@@ -0,0 +1,27 @@
+library tests;
+
+import 'dart:html';
+import 'dart:json';
+import 'package:unittest/unittest.dart';
+import 'package:unittest/html_config.dart';
+
+import 'package:js/js.dart' as js;
+import 'package:weatherApp/weather_station_model.dart';
+
+injectSource(code) {
+  final script = new ScriptElement();
+  script.type = 'text/javascript';
+  script.innerHtml = code;
+  document.body.nodes.add(script);
+}
+
+main() {
+  useHtmlConfiguration();
+
+  test('Anemometer line parse test', () {
+    var line='2013/06/14,00:10.00,243,249,255,13.97,16.71,19.92,600';
+    AnemometerReading value = new AnemometerReading.parse(line);
+    expect(value.backed, equals(243));
+  });
+
+}
