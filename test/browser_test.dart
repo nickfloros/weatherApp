4333cbc4e4659adbfb3a627c30d90d76ffbe38d5
nickfloros <nickfloros@gmail.com>
2013-06-22 21:35:43 +0100

    more comments

--- test/browser_test.dart
+++ test/browser_test.dart
@@ -18,6 +18,7 @@ injectSource(code) {
 main() {
   useHtmlConfiguration();
 
+ 
   test('Anemometer line parse test', () {
     var line='2013/06/14,00:10.00,243,249,255,13.97,16.71,19.92,600';
     AnemometerReading value = new AnemometerReading.parse(line);
